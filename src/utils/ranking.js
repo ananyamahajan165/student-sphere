d9f4a1b130ba89de227b60231bce87f1fdf9a908
Your Name <ananyam054@gmail.com>
2026-04-02 10:37:59 +0530

    Updated

---
+++ src/utils/ranking.js
@@ -0,0 +1,29 @@
+function getGrade(marks) {
+  if (marks >= 90) return "A+";
+  if (marks >= 75) return "A";
+  if (marks >= 60) return "B";
+  if (marks >= 40) return "C";
+  return "F";
+}
+
+function calculateRank(students) {
+  // sort descending
+  students.sort(function (a, b) {
+    return b.marks - a.marks;
+  });
+
+  var rank = 1;
+
+  for (var i = 0; i < students.length; i++) {
+    if (i > 0 && students[i].marks < students[i - 1].marks) {
+      rank = i + 1;
+    }
+
+    students[i].rank = rank;
+    students[i].grade = getGrade(students[i].marks);
+  }
+
+  return students;
+}
+
+module.exports = { calculateRank };
