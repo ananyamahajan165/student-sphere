204767581d8a0073f6e63c6bdf2128cdf15fe474
Your Name <ananyam054@gmail.com>
2026-04-03 13:31:55 +0530

    Updated

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
 const express = require("express");
-const path = require("path");
+const cors = require("cors");
 const studentRoutes = require("./routes/student.routes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
-app.use("/api/students", studentRoutes);
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
+// routes
+app.use("/api/students", studentRoutes);
 
 module.exports = app;
