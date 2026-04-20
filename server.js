b6697f4b5a07d93a761c627c224bbe6b54145341
Your Name <ananyam054@gmail.com>
2026-04-20 12:54:03 +0530

    Update server and app logic

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = require("./src/app");
 
 const PORT = 5001;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const studentRoutes = require("./routes/studentRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public")); // 👈 IMPORTANT
+
+
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
-  res.send("SERVER WORKING");
+  res.sendFile(path.join(__dirname, "../public/login.html"));
 });
 
+// API routes
 app.use("/api/students", studentRoutes);
 
 module.exports = app;
