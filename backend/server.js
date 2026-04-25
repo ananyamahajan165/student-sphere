00f5d00e4a7f7e57540dcd7783f6f7408e1417bf
Your Name <ananyam054@gmail.com>
2026-04-25 17:24:47 +0530

    feat: connect backend to MongoDB using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-// Entry point for backend server
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+    res.send("Backend running");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
