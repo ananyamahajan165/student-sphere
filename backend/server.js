fe1d218ef57afd08be55e39c0aa2fc740dc7dab8
Your Name <ananyam054@gmail.com>
2026-04-27 20:34:51 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-
 const app = express();
-app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
-
-
-const studentRoutes = require('./routes/studentRoutes');
-app.use('/api/students', studentRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Backend running");
+  res.send("Server Running");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(5000, () => {
+  console.log("Server started");
+});
